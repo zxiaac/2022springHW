b9338d11dbd103f73cd1f6c772c8b8f942b76c7d
root <root@localhost.localdomain>
2022-02-28 01:15:02 -0500

    add results for h2

--- h2.sh
+++ h2.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-
+ 
 NAME='ecut'
-RUNDIR='$HOME/QE'
+RUNDIR='/root/q-e'
 
 for CUTOFF in 5 10 15 20 25 30
 do
 cat > $RUNDIR/$NAME.$CUTOFF.in << EOF
 &control
-	calculation = 'scf',
+	calculation = 'relax',
 	prefix = '$PREFIX',
-	pseudo_dir = '$RUNDIR',
-	outdir = '/tmp/',
+	pseudo_dir = '$RUNDIR/pseudo',
 	verbosity = 'high'
 /
 &system
@@ -25,15 +24,18 @@ cat > $RUNDIR/$NAME.$CUTOFF.in << EOF
 &electrons
 	conv_thr = 1e-8
 /
+&ions
+	ion_dynamics='bfgs'
+/
 ATOMIC_SPECIES
  H 1.00784 H_US.van
-ATOMIC_POSITIONS (angstron)
+ATOMIC_POSITIONS (angstrom)
  H 0.00 0.00 0.00
  H 1.00 0.00 0.00
 K_POINTS (automatic)
  1 1 1 1 1 1
 EOF
 
-$HOME/pw.x < $RUNDIR/$NAME.$CUTOFF.in > $NAME.$CUTOFF.out
-rm -R /tmp/H_work*
+$HOME/q-e/bin/pw.x < $RUNDIR/$NAME.$CUTOFF.in > $NAME.$CUTOFF.out
+#rm -R /tmp/H_work*
 done
