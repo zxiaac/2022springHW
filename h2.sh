a46ce955c4fb1ba740f54e41e4c0faa0af65fc95
root <root@localhost.localdomain>
2022-02-28 13:43:45 -0500

    update for p3

--- h2.sh
+++ h2.sh
@@ -5,7 +5,9 @@ RUNDIR='/root/q-e'
 
 for CUTOFF in 5 10 15 20 25 30
 do
-cat > $RUNDIR/$NAME.$CUTOFF.in << EOF
+	for DIST in 0.7 0.72 0.74 0.76 0.78 0.8 0.82 0.84 0.86
+	do
+	cat > $RUNDIR/$NAME.$CUTOFF.$DIST.in << EOF
 &control
 	calculation = 'relax',
 	prefix = '$PREFIX',
@@ -31,11 +33,12 @@ ATOMIC_SPECIES
  H 1.00784 H_US.van
 ATOMIC_POSITIONS (angstrom)
  H 0.00 0.00 0.00
- H 1.00 0.00 0.00
+ H $DIST 0.00 0.00
 K_POINTS (automatic)
  1 1 1 1 1 1
 EOF
 
-$HOME/q-e/bin/pw.x < $RUNDIR/$NAME.$CUTOFF.in > $NAME.$CUTOFF.out
-#rm -R /tmp/H_work*
+	$HOME/q-e/bin/pw.x < $RUNDIR/$NAME.$CUTOFF.$DIST.in > $NAME.$CUTOFF.$DIST.out
+	#rm -R /tmp/H_work*
+	done
 done
